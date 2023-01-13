a4056ff8c3c92a3d7fb79bf263b8528534fa465c
jayvardhan <jayvardhanprajapat@gmail.com>
2023-01-13 17:11:02 +0530

    configured vpc

--- main.tf
+++ main.tf
@@ -1,10 +1,149 @@
-provider "aws" {
 
-  region = "ap-south-1"
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    "Name" = "custom-VPC"
+  }
+}
+
+# Public subnets 
+
+resource "aws_subnet" "public1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "ap-south-1a"
+  tags = {
+    "Name" = "public-1"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1b"
+  tags = {
+    "Name" = "public-2"
+  }
+}
+
+# Private subnets
+
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1a"
+  tags = {
+    "Name" = "private-1"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-south-1b"
+  tags = {
+    "Name" = "private-2"
+  }
+}
+
+
+# Internet Gateway
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
 
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0cca134ec43cf708f"
-  instance_type = "t2.micro"
+
+# Route  Table 
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    "Name" = "public-route-table"
+  }
+}
+
+# Route Association 
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+
+
+resource "aws_eip" "nat1" {
+  vpc = true
+}
+
+resource "aws_eip" "nat2" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat1" {
+  allocation_id = aws_eip.nat1.id
+  subnet_id     = aws_subnet.public1.id
+}
+resource "aws_nat_gateway" "nat2" {
+  allocation_id = aws_eip.nat2.id
+  subnet_id     = aws_subnet.public2.id
+}
+
+
+# Private Route table
+
+
+resource "aws_route_table" "private1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat1.id
+  }
+  tags = {
+    "Name" = "private-route-table-1"
+  }
+}
+
+# Route Association 
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private1.id
+}
+
+
+
+
+resource "aws_route_table" "private2" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat2.id
+  }
+   tags = {
+    "Name" = "private-route-table-2"
+  }
+}
+
+
+
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private2.id
 }

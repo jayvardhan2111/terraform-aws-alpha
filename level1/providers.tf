33da862809cd745afb5b6ecf4fe0406b11b86bdf
Jayvardhan Prajapat <103930724+jayvardhan2111@users.noreply.github.com>
2023-02-28 09:32:10 +0530

    Update providers.tf

--- level1/providers.tf
+++ level1/providers.tf
@@ -7,7 +7,7 @@ terraform {
     bucket = "terraform-remote-state-230223"
     key    = "key/level1.tfstate"
     region = "ap-south-1"
-    # dynamodb_table = "terraform-remote-state-lock"
+    dynamodb_table = "terraform-remote-state-lock"
 
   }
 }

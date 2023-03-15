4422d932a2febf7aa16bdcd876148b63ffabc017
Jayvardhan Prajapat <103930724+jayvardhan2111@users.noreply.github.com>
2023-03-15 10:30:11 +0530

    Update iam.tf

--- level2/iam.tf
+++ level2/iam.tf
@@ -1,48 +1,52 @@
-resource "aws_iam_policy" "s3_policy" {
+resource "aws_iam_policy" "s3-FullAcess-policy" {
   name        = var.env_code
-  path        = "/"
-  
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "s3:*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-resource "aws_iam_role" "main" {
-  name                = var.env_code
-  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"]
+  description = "Allow full access to s3"
 
-  assume_role_policy = <<EOF
+  policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
         {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-                "Service": "ec2.amazonaws.com"
-            },
             "Effect": "Allow",
-            "Sid": ""
+            "Action": [
+                "s3:*",
+                "s3-object-lambda:*"
+            ],
+            "Resource": "*"
         }
     ]
 }
 EOF
 }
-resource "aws_iam_instance_profile" "main" {
-  name = var.env_code
-  role = aws_iam_role.main.name
+
+resource "aws_iam_role" "s3Role" {
+  name = "s3-Role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_iam_role_policy_attachment" "test-attach" {
   role       = aws_iam_role.s3Role.name
   policy_arn = aws_iam_policy.s3-FullAcess-policy.arn
+}
+
+resource "aws_iam_instance_profile" "my-inst-profile" {
+  name = var.env_code
+  role = aws_iam_role.s3Role.name
 }
 
 

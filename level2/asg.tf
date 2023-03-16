b6b5de2e5ae12fb36c591badc86461276e769a6c
jayvardhan <jayvardhanprajapat@gmail.com>
2023-03-17 00:16:17 +0530

    added modules

--- level2/asg.tf
+++ level2/asg.tf
@@ -1,32 +1,15 @@
-resource "aws_launch_configuration" "main" {
-  
-  name_prefix = "$(var.env_code)"
-  image_id = data.aws_ami.amazonlinux.id
-  instance_type = "t2.micro"
-  key_name = "awskey"
-  user_data = file("user_data.sh")
-  security_groups = [aws_security_group.private.id]
-  iam_instance_profile = aws_iam_instance_profile.main.name
+
+module "asg" {
+
+  source = "../modules/asg"
+
+  env_code         = var.env_code
+  vpc_id           = data.terraform_remote_state.level1.outputs.vpc_id
+  subnet_id        = data.terraform_remote_state.level1.outputs.private_subnet_ids
+  lb_sg_id         = module.load-balancer.load_balancer_sg
+  target_group_arn = module.load-balancer.target_group_arn
 
 }
 
-resource "aws_autoscaling_group" "main" {
-  name = var.env_code
-
-  min_size         = 1
-  desired_capacity = 1
-  max_size         = 2
-
-  target_group_arns    = [aws_lb_target_group.main.arn]
-  launch_configuration = aws_launch_configuration.main.name
-  vpc_zone_identifier  = data.terraform_remote_state.level1.outputs.private_subnet_ids
-
-  tag {
-    key                 = "Name"
-    value               = var.env_code
-    propagate_at_launch = true
-  }
-
-}
 
 
